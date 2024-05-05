a78fd7ad755baf919141669ec8196a80703e4020
GerarFR21 <167644748+GerarFR21@users.noreply.github.com>
2024-05-05 07:39:29 -0600

    Add files via upload

---
+++ demo-FR24012/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,18 @@
+package com.iis.FR24012;;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public String Hello() {
+		return "Hello World";
+	}
+	
+    public static void main( String[] args )
+    {
+    	App hw = new App();
+    	System.out.println(hw.Hello());
+    }
+}
